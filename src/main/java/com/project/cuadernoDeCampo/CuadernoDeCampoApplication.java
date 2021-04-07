96ce00c3edb8a0cb3a502d7eeb64e40ccfb80ac1
Guillermo <rollerguille@outlook.es>
2021-04-07 16:25:15 +0200

    Project structure
    Basic Project structure, whit basic dependencies

---
+++ src/main/java/com/project/cuadernoDeCampo/CuadernoDeCampoApplication.java
@@ -0,0 +1,13 @@
+package com.project.cuadernoDeCampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuadernoDeCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuadernoDeCampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cuadernoDeCampo/CuadernoDeCampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cuadernoDeCampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuadernoDeCampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
