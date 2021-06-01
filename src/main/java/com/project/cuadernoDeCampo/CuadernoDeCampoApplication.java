05b5608a685fd4b3c6a7df14109f1cdfbbad6b84
Guillermo <rollerguille@outlook.es>
2021-06-01 10:40:22 +0200

    fixbug branch. Rename urls...

--- src/main/java/com/project/cuadernoDeCampo/CuadernoDeCampoApplication.java
+++ src/main/java/com/project/cuadernoDeCampo/CuadernoDeCampoApplication.java
@@ -2,9 +2,16 @@ package com.project.cuadernoDeCampo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CuadernoDeCampoApplication {
+public class CuadernoDeCampoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CuadernoDeCampoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CuadernoDeCampoApplication.class, args);
